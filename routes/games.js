1330854bbc9f738a7699d0cae02af0afc7f1dc8b
Floudyx <149321354+Floudyx@users.noreply.github.com>
2024-05-17 00:30:43 +0700

    Revert "Дэзэшечка на проверку"

--- routes/games.js
+++
@@ -1,9 +0,0 @@
-const { sendAllGames, deleteGame, addGameController } = require("../controllers/games");
-const { getAllGames } = require("../middlewares/games");
-const gamesRouter = require("express").Router();
-
-gamesRouter.get("/games", getAllGames, sendAllGames);
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
-gamesRouter.post("/games", getAllGames, addGameController) 
-
-module.exports = gamesRouter;
